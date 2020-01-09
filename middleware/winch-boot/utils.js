8055d4210696d17b56f361cb7e5a602807114e18
Fabio <fabiofrancesco.valenti@gmail.com>
2020-01-09 09:16:22 +0100

    [FV] plants boot with WP1 custom ids

--- middleware/winch-boot/utils.js
+++ middleware/winch-boot/utils.js
@@ -1,6 +1,27 @@
 "use strict"
 
+// private part
+//
+function applyDefault (mapping, target) {
+  const prefixKey = `|${target.project.id}|${target.project.code}|`
 
+  let lastIndex = mapping.get(prefixKey);
+  if (lastIndex === undefined) {
+    lastIndex = 0
+  }
+  lastIndex++;
+  mapping.set(prefixKey, lastIndex);
+
+  target._id = `${prefixKey}${lastIndex}|`
+}
+
+function applyWP1Sl (mapping, target) {
+  target._id = `|${target.project.id}|${target.project.code}|${mapping[target.name]}|`
+}
+
+
+// public part
+//
 function buildFeaturesCollection(lat, lng) {
   const result = {
     type: 'FeatureCollection',
@@ -18,23 +39,32 @@ function buildFeaturesCollection(lat, lng) {
   return result;
 }
 
-
 class PlantIdGenerator {
   constructor() {
     this._mapping = new Map();
+    this._wp1Mapping = {
+      'Bafodia': 69,
+      'Batkanu': 7,
+      'Fintonia': 46,
+      'Kagbere': 8,
+      'Kamaranka': 27,
+      'Kathantha Yimboi': 5,
+      // TODO koinadugu
+      'Mabang': 43,
+      'Mara': 31,
+      'Musaia': 65,
+      'Rokonta': 42,
+      'Sinkunia': 55,
+      'Yiffin': 87,
+      };
   }
 
   apply (target) {
-    const prefixKey = `|${target.project.id}|${target.project.code}|`
-
-    let lastIndex = this._mapping.get(prefixKey);
-    if (lastIndex === undefined) {
-      lastIndex = 0
+    if (target.project.id === 'WP1' && target.project.code === 'SLL_2019_001') {
+      applyWP1Sl(this._wp1Mapping, target);
+    } else {
+      applyDefault(this._mapping, target);
     }
-    lastIndex++;
-    this._mapping.set(prefixKey, lastIndex);
-
-    target._id = `${prefixKey}${lastIndex}|`
   }
 }
 
