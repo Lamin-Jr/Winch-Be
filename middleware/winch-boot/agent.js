24981e6e21eb51b45e041c0a88ed06689c86d453
giuseppe1686 <pginformaticame@gmail.com>
2020-01-27 19:24:08 +0100

    [GP] agent refactor agent boot

--- middleware/winch-boot/agent.js
+++ middleware/winch-boot/agent.js
@@ -13,40 +13,43 @@ module.exports.buildAgents = () => {
 
       const agents = agentByPlantIdEntry[1](agentByPlantIdEntry[0]);
 
-      agents.forEach(agent => {
+      agents.forEach((agent, index) => {
 
         Agent.create(agent)
           .then(createResult => {
             console.info(`agent creation succeeded with id: ${createResult._id}`);
-
-            Plant.updateOne({
-              _id: agentByPlantIdEntry[0]
-            }, {
-              agents: agents
-            })
-              .then(plantUpdateResult => {
-                console.info(`'${plantUpdateResult}' plant update succeeded`);
-              })
-              .catch(plantUpdateError => {
-                console.error(`'[${agentId}]' agent creation error: ${plantUpdateError}`);
-                reject(createError);  
-              });
           })
           .catch(createError => {
             if (createError.name === 'MongoError' && createError.code === 11000) {
-              console.log(`'[${agentId}]' agent creation already done`);
+              console.log(`'${agent.fullName}@${agentByPlantIdEntry[0]}' agent creation already done`);
             } else {
-              console.error(`'[${agentId}]' agent creation error: ${createError}`);
+              console.error(`'${agent.fullName}@${agentByPlantIdEntry[0]}' agent creation error: ${createError}`);
               reject(createError);
             }
           })
           .finally(() => {
-            // TODO controllare length
-            resolve();
+            if (index === agents.length - 1) {
+              Plant.updateOne({
+                _id: agentByPlantIdEntry[0]
+              }, {
+                $set: { 'organization.agents': agents }
+              })
+                .then(plantUpdateResult => {
+                  console.info(`'${agentByPlantIdEntry[0]}' plant update (${plantUpdateResult.nModified}) succeeded`);
+                  resolve();
+
+                })
+                .catch(plantUpdateError => {
+                  console.error(`'${agentByPlantIdEntry[0]}' plant update error: ${plantUpdateError}`);
+                  reject(plantUpdateError);
+                });
+
+            } 
+
           });
     
       });
-
+      
     });
 
   });
@@ -57,16 +60,16 @@ function getAgentsByPlantId() {
   return {
     '|BEN|BEN_2019_005|1|': (plantId) => {
       return [
-        buildAgent('Geraud Fadeyi', buildAgentContacts('+22967337540'), plantId)
+        buildAgent('5e2f29e2b0a5102704b89d0a', 'Geraud Fadeyi', buildAgentContacts('+22967337540'), plantId)
       ];
     },
   };
 }
 
 
-function buildAgent(fullName, contacts, plantId) {
+function buildAgent(idAsString, fullName, contacts, plantId) {
   const result = {
-    _id: new mongoose.Types.ObjectId(),
+    _id: new mongoose.Types.ObjectId(idAsString),
     fullName: fullName,
     contacts: contacts,
     plant: plantId,
